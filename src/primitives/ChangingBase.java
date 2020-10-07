4bd0e91ec0678bce8c8365cbe29e79ca4fedd046
Sunny Park <sunnypark@sunnys-air.lan>
2020-10-06 20:16:44 -0700

    Add 2 solutions for primitives

--- src/primitives/ChangingBase.java
+++ src/primitives/ChangingBase.java
@@ -1,7 +1,66 @@
 package primitives;
 
 public class ChangingBase {
-	public static void main(String[] args) {
+	/**
+	 * q. given an integer(represented as a string) under base b1, convert it to base b2.
+	 * 
+	 * sol.
+	 * convert b1 -> 10, and then 10 -> b2
+	 */
+	public static String changeBase(String input, int b1, int b2) {
+		return convertTenToN(convertNToTen(input, b1), b2);
+	}
+	
+	/**
+	 * accum properly with calculating the weight
+	 */
+	private static String convertNToTen(String input, int base) {
+		int total = 0;
 		
+		for (int i = input.length() - 1; i >= 0; i--) {
+			int weight = input.length() - 1 - i;
+			char curChar = input.charAt(i);
+			int curVal = 0;
+			if (Character.isDigit(curChar)) {
+				curVal = Character.getNumericValue(curChar);
+			} else {
+				curVal = curChar - 'A' + 10;
+			}
+			total += curVal * (int) Math.pow(base, weight);
+		}
+		return String.valueOf(total);
+	}
+	
+	/**
+	 * loop while the remaining num is greater than 0
+	 * append remainder and reverse at the end
+	 * 
+	 * e.g. 
+	 * 10 / 2 = 5 ... 0
+	 * 5 / 2 = 2 ... 1
+	 * 2 / 2 = 1 ... 0
+	 * 1 / 2 = 0 ... 1
+	 */
+	private static String convertTenToN(String input, int base) {
+		StringBuilder sb = new StringBuilder();
+		int curVal = Integer.parseInt(input);
+
+		while (curVal > 0) {
+			int remainder = curVal % base;
+			if (remainder < 10) {
+				sb.append(remainder);
+			} else {
+				sb.append((char)(remainder - 10 + 'A'));
+			}
+			
+			curVal = curVal / base;
+		}
+		return sb.reverse().toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(changeBase("12", 10, 2));
+		System.out.println(changeBase("123", 4, 16));
+		System.out.println(changeBase("123", 10, 10));
 	}
 }
---
+++ src/primitives/PowerOfFour.java
@@ -0,0 +1,38 @@
+package primitives;
+
+public class PowerOfFour {
+
+	/**
+	 * q. validate if input is power of 4.
+	 * 
+	 * sol.
+	 * power of fours (base 2) =>  1, 100(=4), 10000(=16), 1000000(4^3=2^6=64)
+	 * 
+	 * 1) nonZero
+	 * 2) should have single leading one bit 
+	 *    => e.g. input = 100, then (input-1) = 011
+	 * 3) should odd positioned bits 
+	 *    => 1 or 3 or 5 or 7 or ... 
+	 *    => (base2) 1010101...1 // (base16)0x55555555
+	 *    
+	 *    => 7 6 5 4 | 3 2 1 0
+	 *    => 0 1 0 1 | 0 1 0 1
+	 *    
+	 *    =>  16 * 5 | 16^0  * 5
+	 */
+	public static boolean isPowerOfFour(int input) {
+		int mask = 0x55555555;
+		
+		boolean isNonZero = input != 0;
+		boolean hasSingleLeadingOneBit = (input & (input - 1)) == 0;
+		boolean hasOddPositionedBits = (input & mask) == input;
+		
+		return isNonZero && hasSingleLeadingOneBit && hasOddPositionedBits;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isPowerOfFour(16)); // true
+		System.out.println(isPowerOfFour(8)); // false
+	}
+
+}
